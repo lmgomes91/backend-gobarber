4286f987572b8280ce7ab5cb5cb1f5b3cff04e2a
LucasGomes <lgomes@post.com>
2020-04-21 03:01:36 -0300

    GoStack Nivel 02 - Cadastrando Usuarios

--- src/database/migrations/1587443944574-CreateUsers.ts
+++ src/database/migrations/1587443944574-CreateUsers.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateAppointments1587409522572 implements MigrationInterface {
+export default class CreateUsers1587443944574 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
@@ -15,14 +15,17 @@ export class CreateAppointments1587409522572 implements MigrationInterface {
                         default: 'uuid_generate_v4()',
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
-                        isNullable: false
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
-                        isNullable: false
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
                     },
                     {
                         name: 'created_at',
@@ -41,7 +44,7 @@ export class CreateAppointments1587409522572 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments')
+        await queryRunner.dropTable('users')
     }
 
 }
