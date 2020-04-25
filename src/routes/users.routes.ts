3a033c99e7044edf1256890c03afaa64d0771a1e
LucasGomes <lgomes@post.com>
2020-04-25 00:41:04 -0300

    GoStack Nivel 02 - Modulo Finalizado

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,30 +1,50 @@
 import { Router } from 'express'
+import multer from 'multer'
+import uploadConfig from '../config/upload'
 
 import CreateUserService from '../services/CreateUserService'
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
+import ensureAthenticated from '../middlewares/ensureAthenticated'
 
 
 const usersRouter = Router()
-
+const upload = multer(uploadConfig)
 
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body
+  const { name, email, password } = request.body
 
-    const createUser = new CreateUserService()
+  const createUser = new CreateUserService()
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password
+  const user = await createUser.execute({
+    name,
+    email,
+    password
+  })
+
+  delete user.password
+
+  return response.json(user)
+
+})
+
+usersRouter.patch(
+  '/avatar',
+  ensureAthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService()
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename
     })
 
     delete user.password
 
-    return response.json(user)
-  } catch (error) {
-    return response.status(400).json({ error: error.message })
-  }
-})
+    return response.json({ user })
+
+
+  })
 
 export default usersRouter
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -3,6 +3,8 @@ import AppointmenstRepository from '../repositories/AppointmentsRepository'
 
 import { startOfHour } from 'date-fns'
 import { getCustomRepository } from 'typeorm'
+import AppError from '../errors/AppError'
+
 
 interface Request {
   provider_id: string
@@ -24,7 +26,7 @@ class CreateAppointmentService {
       findBydate(appointmentDate)
 
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked')
+      throw new AppError('This appointment is already booked')
     }
 
     const appointment = appointmentsRepository.create({
